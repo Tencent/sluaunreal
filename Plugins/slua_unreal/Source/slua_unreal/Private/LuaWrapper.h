bc3f2a6382a4d111d1bc86f5c6bef23a109c6fb4
easily <yili19890109@gmail.com>
2018-02-26 16:08:05 +0800

    LuaWrapper

---
+++ Plugins/slua_unreal/Source/slua_unreal/Private/LuaWrapper.h
@@ -0,0 +1,18 @@
+#include "LuaObject.h"
+#include "Log.h"
+#include <map>
+
+#define LUA_WRAPPER_DEBUG
+
+namespace slua {
+
+	struct LuaWrapper {
+
+		static void init(lua_State* L);
+		static int pushValue(lua_State* L, UStructProperty* p, UScriptStruct* uss, uint8* parms);
+		static int checkValue(lua_State* L, UStructProperty* p, UScriptStruct* uss, uint8* parms, int i);
+
+	};
+
+}
+

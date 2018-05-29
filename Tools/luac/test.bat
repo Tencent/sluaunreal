376d1b4a0c453901a6cad7f93b277e1bb091cf14
leeyi <leeyi@tencent.com>
2018-05-29 18:24:27 +0800

    luac tools

--- Tools/luac/test.bat
+++ Tools/luac/test.bat
@@ -1,8 +1,11 @@
-lua test.lua
+x86\lua test.lua
 x64\lua test.lua
-luac -o test.luac test.lua
+
+x86\luac -o x86\test.luac test.lua
 x64\luac -o x64\test.luac test.lua
-lua test.luac
+
+x86\lua x86\test.luac
 x64\lua x64\test.luac
-lua x64\test.luac
-x64\lua test.luac
+
+x86\lua x64\test.luac
+x64\lua x86\test.luac

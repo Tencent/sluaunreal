a6cca8131653237becda3c9fe67f68db55f0587e
leeyi <leeyi@tencent.com>
2018-08-14 15:58:22 +0800

    add debugger code license

--- Plugins/slua_unreal/Source/slua_unreal/Private/LuaDebugExtension.cpp
+++ Plugins/slua_unreal/Source/slua_unreal/Private/LuaDebugExtension.cpp
@@ -1,3 +1,16 @@
+// Tencent is pleased to support the open source community by making sluaunreal available.
+
+// Copyright (C) 2018 THL A29 Limited, a Tencent company. All rights reserved.
+// Licensed under the BSD 3-Clause License (the "License"); 
+// you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+// https://opensource.org/licenses/BSD-3-Clause
+
+// Unless required by applicable law or agreed to in writing, 
+// software distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and limitations under the License.
+
 #include "LuaObject.h"
 #include "LuaSocket/luasocket.h"
 
@@ -10,12 +23,12 @@ namespace slua {
 
     void initDebugExtension(lua_State *L){
 		AutoStack autoStack(L);
-		//初始化Debug插件
+		
 		lua_pushglobaltable(L); // _G
 		lua_newtable(L);		// _G, t
-		lua_pushvalue(L, -1);	// _G, t, t 拷贝一个t
+		lua_pushvalue(L, -1);	// _G, t, t
 		lua_setfield(L, -3, "lua_extension"); // _G, t
-		//引入luasocket
+		
         lua_pushcfunction(L, luasocket);   // _G, t, f
 		lua_setfield(L, -2, "luasocket");  // _G, t
     }

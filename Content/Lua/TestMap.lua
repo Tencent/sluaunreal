ab0b59fd72181ce8344bdc556c60581a8d99db4d
leeyi <leeyi@tencent.com>
2018-08-14 16:00:25 +0800

    LuaMap test case

--- Content/Lua/TestMap.lua
+++ Content/Lua/TestMap.lua
@@ -1,60 +1,40 @@
 local Test=import('SluaTestCase');
 local t=Test();
 
-local map = t:GetMap()
-map:Add(5,"500")
-print("map:Get(5)", map:Get(5))
+local t = {t:GetMap(), slua.Map(UEnums.EPropertyClass.Int, UEnums.EPropertyClass.Str)}
+for i,v in ipairs(t) do
+    local map = v
+    map:Add(5,"500")
+    print("map:Get(5)", map:Get(5))
 
-map:Add(1,"100")
-map:Add(2,"200")
-print("map num", map:Num())
+    map:Add(1,"100")
+    map:Add(2,"200")
+    print("map num", map:Num())
 
-map:Add(8,"800")
-map:Add(9,"900")
-print("map num", map:Num())
+    map:Add(8,"800")
+    map:Add(9,"900")
+    print("map num", map:Num())
 
-print("map get(1)", map:Get(1))
-print("map get(2)", map:Get(2))
+    print("map get(1)", map:Get(1))
+    print("map get(2)", map:Get(2))
 
-print("map remove(1)", map:Remove(1))
-print("map num", map:Num())
+    print("map remove(1)", map:Remove(1))
+    print("map num", map:Num())
 
-print("map:Get(8)", map:Get(8))
-print("map:Get(9)", map:Get(9))
-print("map num", map:Num())
+    print("map:Get(8)", map:Get(8))
+    print("map:Get(9)", map:Get(9))
 
-map:Clear()
-print("map num", map:Num())
+    local v,r = map:Get(100)
+    print("map:Get(100)", v, r)
 
-local map = slua.Map(UEnums.EPropertyClass.Int, UEnums.EPropertyClass.Str)
-map:Add(5,"500")
-print("map:Get(5)", map:Get(5))
+    print("map num", map:Num())
 
-map:Add(1,"100")
-map:Add(2,"200")
-print("map num", map:Num())
+    print("foreach begin...")
+    for k,v in map:Pairs() do
+        print(k,v)
+    end
+    print("foreach end...")
 
-map:Add(8,"800")
-map:Add(9,"900")
-print("map num", map:Num())
-
-print("map get(1)", map:Get(1))
-print("map get(2)", map:Get(2))
-
-print("map remove(1)", map:Remove(1))
-print("map num", map:Num())
-
-print("map:Get(8)", map:Get(8))
-print("map:Get(9)", map:Get(9))
-print("map num", map:Num())
-
-print("foreach begin...")
-for k,v in map:Pairs() do
-    print(k,v)
+    map:Clear()
+    print("map num", map:Num())
 end
-print("foreach end...")
-
-map:Clear()
-print("map num", map:Num())
-
-

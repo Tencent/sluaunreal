7369de6c15f65146928cb612526ac433f5d54a38
pangweiwei <sineysan@163.com>
2019-01-05 12:52:56 +0800

    add arm64 liblua.a and build file

--- Plugins/slua_unreal/make_android.sh
+++ Plugins/slua_unreal/make_android.sh
@@ -11,6 +11,14 @@ cmake --build build_android_v7a --config Release
 mkdir -p Library/Android/armeabi-v7a/
 cp build_android_v7a/liblua.a Library/Android/armeabi-v7a/liblua.a
 
+mkdir -p build_android_arm64 && cd build_android_arm64
+cmake -DANDROID_ABI=arm64-v8a -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=aarch64-linux-android-4.9 -DANDROID_NATIVE_API_LEVEL=android-21 ..
+cd ..
+
+cmake --build build_android_arm64 --config Release
+mkdir -p Library/Android/armeabi-arm64/
+cp build_android_arm64/liblua.a Library/Android/armeabi-arm64/liblua.a
+
 mkdir -p build_android_x86 && cd build_android_x86
 cmake -DANDROID_ABI=x86 -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=x86-4.9 -DANDROID_NATIVE_API_LEVEL=android-9 ..
 cd ..

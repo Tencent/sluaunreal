82a54938e6e00667ac19f1e048f48d76b40eb131
leeyi <leeyi@tencent.com>
2019-01-24 18:45:48 +0800

    prop link test case

--- Content/Lua/TestStruct.lua
+++ Content/Lua/TestStruct.lua
@@ -5,3 +5,55 @@ local b = FVector(2,2,2)
 local c = a+b*3.14
 local d = FVector(1,1,1)*3.14*2+FVector(1,1,1)
 assert(c==d)
+
+if FLinkStruct then
+    st = FLinkStruct()
+    b2d = st.b2d
+    min = b2d.Min
+
+    print("------")
+    print(1, min.X)
+    print(2, b2d.Min.X)
+    print(3, st.b2d.Min.X)
+    collectgarbage("collect")
+
+    print("------")
+    min.X = 100
+    print(1, min.X)
+    print(2, b2d.Min.X)
+    print(3, st.b2d.Min.X)
+    collectgarbage("collect")
+
+    print("------")
+    b2d.Min.X = 200
+    print(1, min.X)
+    print(2, b2d.Min.X)
+    print(3, st.b2d.Min.X)
+    collectgarbage("collect")
+
+    print("------")
+    st.b2d.Min.X = 300
+    print(1, min.X)
+    print(2, b2d.Min.X)
+    print(3, st.b2d.Min.X)
+    collectgarbage("collect")
+
+    b2d = nil
+    collectgarbage("collect")
+
+
+    print("------")
+    local ret,err=pcall(function()
+        print(1, min.X)
+    end)
+    print(ret,err)
+
+    st = nil
+    collectgarbage("collect")
+
+    print("------")
+    local ret,err=pcall(function()
+        print(2, b2d.Min)
+    end)
+    print(ret,err)
+end

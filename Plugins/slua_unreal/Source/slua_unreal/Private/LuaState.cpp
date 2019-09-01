d20e2f9cc64d201b60b0b40c24650551cbc4d2cc
Qizhiqiang <hbqzq@qq.com>
2019-09-01 11:56:08 +0800

    Fixed compiling error when calling LuaState::SetErrorDelegate()

--- Plugins/slua_unreal/Source/slua_unreal/Private/LuaState.cpp
+++ Plugins/slua_unreal/Source/slua_unreal/Private/LuaState.cpp
@@ -315,6 +315,10 @@ namespace NS_SLUA {
 		loadFileDelegate = func;
 	}
 
+	void LuaState::setErrorDelegate(ErrorDelegate func) {
+		errorDelegate = func;
+	}
+
 	static void* findParent(GenericUserData* parent) {
 		auto pp = parent;
 		while(true) {

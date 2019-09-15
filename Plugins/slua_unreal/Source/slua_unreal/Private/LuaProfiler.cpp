99365616bd07094ece2b3f6bbdfdde79955be7df
siney <sineysan@163.com>
2019-09-15 23:27:23 +0800

    move variant to anonymous name space

--- Plugins/slua_unreal/Source/slua_unreal/Private/LuaProfiler.cpp
+++ Plugins/slua_unreal/Source/slua_unreal/Private/LuaProfiler.cpp
@@ -33,16 +33,16 @@ namespace NS_SLUA {
 		CONNECTED = 1,
 	};
 
-	LuaVar selfProfiler;
-	bool ignoreHook = false;
-	HookState currentHookState = HookState::UNHOOK;
-	int64_t profileTotalCost = 0;
-	bool openAttachMode = true;
-	p_tcp tcpSocket = nullptr;
-	const char* ChunkName = "[ProfilerScript]";
-	static FArrayWriter s_messageWriter;
-
 	namespace {
+
+		LuaVar selfProfiler;
+		bool ignoreHook = false;
+		HookState currentHookState = HookState::UNHOOK;
+		int64_t profileTotalCost = 0;
+		bool openAttachMode = true;
+		p_tcp tcpSocket = nullptr;
+		const char* ChunkName = "[ProfilerScript]";
+		static FArrayWriter s_messageWriter;
 
 		void makeProfilePackage(FArrayWriter& messageWriter,
 			int hookEvent, int64_t time,

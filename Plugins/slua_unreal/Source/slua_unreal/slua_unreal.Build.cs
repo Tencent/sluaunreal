4a851e2f5c0d00c32ba473e0d11947df9005e63a
siney <siney@tencent.com>
2020-07-22 11:21:36 +0800

    fix #332

--- Plugins/slua_unreal/Source/slua_unreal/slua_unreal.Build.cs
+++ Plugins/slua_unreal/Source/slua_unreal/slua_unreal.Build.cs
@@ -41,7 +41,11 @@ public class slua_unreal : ModuleRules
         }
         else if (Target.Platform == UnrealTargetPlatform.Android)
         {
-#if UE_4_24_OR_LATER
+#if UE_4_25_OR_LATER
+            PublicAdditionalLibraries.Add(Path.Combine(externalLib, "Android/armeabi-v7a/liblua.a"));
+            PublicAdditionalLibraries.Add(Path.Combine(externalLib, "Android/armeabi-arm64/liblua.a"));
+            PublicAdditionalLibraries.Add(Path.Combine(externalLib, "Android/x86/liblua.a"));
+#elif UE_4_24_OR_LATER
             PublicAdditionalLibraries.Add(Path.Combine(externalLib, "Android/armeabi-arm64/liblua.a"));
             PublicAdditionalLibraries.Add(Path.Combine(externalLib, "Android/armeabi-v7a/liblua.a"));
             PublicAdditionalLibraries.Add(Path.Combine(externalLib, "Android/x86/liblua.a"));

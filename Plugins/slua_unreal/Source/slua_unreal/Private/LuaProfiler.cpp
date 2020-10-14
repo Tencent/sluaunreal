216a09a348bacdc540d157f88556a6de2142ef98
sineypang <sineypang@gmail.com>
2020-10-14 13:33:32 +0800

    fix #381

--- Plugins/slua_unreal/Source/slua_unreal/Private/LuaProfiler.cpp
+++ Plugins/slua_unreal/Source/slua_unreal/Private/LuaProfiler.cpp
@@ -263,7 +263,10 @@ namespace NS_SLUA {
 
 		int changeHookState(lua_State* L) {
 			HookState state = (HookState)lua_tointeger(L, 1);
+
+			if (state == currentHookState) return 0;
 			currentHookState = state;
+
 			if (state == HookState::UNHOOK) {
 //                LuaMemoryProfile::stop();
 				lua_sethook(L, nullptr, 0, 0);

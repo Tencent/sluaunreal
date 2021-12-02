9558b2ece73f0653021e9b87977168511f253165
huanming li <lhm.cold@163.com>
2021-12-02 17:22:43 +0800

    修复一个使用LuaActorComponent，会在初始化时崩溃的bug

--- Plugins/slua_unreal/Source/slua_unreal/Public/LuaActor.h
+++ Plugins/slua_unreal/Source/slua_unreal/Public/LuaActor.h
@@ -180,9 +180,9 @@ class SLUA_UNREAL_API ULuaActorComponent : public UActorComponent, public slua_L
 	};
 protected:
 	virtual void BeginPlay() override {
-		Super::BeginPlay();
 		if (!init(this, "LuaActorComponent", LuaStateName, LuaFilePath)) 
 			return;
+		Super::BeginPlay();
 		if (!GetClass()->HasAnyClassFlags(CLASS_CompiledFromBlueprint))
 			ReceiveBeginPlay();
 		PrimaryComponentTick.SetTickFunctionEnable(postInit("bCanEverTick"));

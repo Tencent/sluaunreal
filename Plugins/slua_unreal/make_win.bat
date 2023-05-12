3897d1326f1274bb8bfc094ac71dc16226a910f1
zjhongxian <zjhongxian@gmail.com>
2023-05-12 10:41:01 +0800

    windows平台lua库增加调试信息

--- Plugins/slua_unreal/make_win.bat
+++ Plugins/slua_unreal/make_win.bat
@@ -1,13 +1,15 @@
 mkdir build_win32 & pushd build_win32
 cmake -G "Visual Studio 16 2019" ..
 popd
-cmake --build build_win32 --config Release
+cmake --build build_win32 --config RelWithDebInfo
 md Library\Win32
-copy /Y build_win32\Release\lua.lib Library\Win32\lua.lib
+copy /Y build_win32\RelWithDebInfo\lua.lib Library\Win32\lua.lib
+copy /Y build_win32\RelWithDebInfo\lua.pdb Library\Win32\lua.pdb
 
 mkdir build_win64 & pushd build_win64
 cmake -G "Visual Studio 16 2019" -A x64 ..
 popd
-cmake --build build_win64 --config Release
+cmake --build build_win64 --config RelWithDebInfo
 md Library\Win64
-copy /Y build_win64\Release\lua.lib Library\Win64\lua.lib
+copy /Y build_win64\RelWithDebInfo\lua.lib Library\Win64\lua.lib
+copy /Y build_win32\RelWithDebInfo\lua.pdb Library\Win64\lua.pdb
